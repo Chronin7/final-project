0d76e8ea99853d21c8b8fb907786ee1ac7d943eb
Chronin7 <liam.perl@ucas-edu.net>
2024-12-03 08:10:07 -0700

    push for liam (dad)

--- flinal_main.py
+++ flinal_main.py
@@ -1,6 +1,20 @@
 from final_ascii import *
 import random
 import time
+import sys
+import termios
+def instant():
+    fd = sys.stdin.fileno()
+    orig = termios.tcgetattr(fd)
+    new = termios.tcgetattr(fd)
+    new[3] = new[3] & ~termios.ICANON
+    new[6][termios.VMIN] = 1
+    new[6][termios.VTIME] = 0
+    try:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, new)
+        return sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, orig)
 def inputcorect():
 	print("thats not a valid input ",end="")
 	if random.randint(1,10):
@@ -9,17 +23,25 @@ def inputcorect():
 		print("just kidding just please input a valid input")
 	else:
 		print()
+def print_mon(hp,nm):
+	print(f"""
+          {nm}
+ ________________________
+| health left: {hp}
+|________________________
+""")
+	return random.randint(1,20) < 14,random.randint(1,20)
 def get(raeraty):
 	if raeraty == 1:
-		return "heath potion",5
+		return "heath potion with healing power of 5", 5
 	if raeraty == 2:
-		return "heath potion",10
+		return "heath potion with healing power of 10",10
 	if raeraty == 3:
-		return "heath potion",20
+		return "heath potion with healing power of 20",20
 	if raeraty == 4:
-		return "heath potion",50
+		return "heath potion with healing power of 50",50
 	if raeraty == 5:
-		return "heath potion",100
+		return "heath potion with healing power of 100",100
 exoshtion = 0
 rations = 5
 monkilled = False
@@ -28,17 +50,11 @@ win = False
 loot = [f"hp{20}"]
 name = ""
 playedmount = 0
+health = 100
 coller = ""
+lootunseen = []
+monlist = ["skeleton","mummy","giant lizard","dragon","flying snake","killer bunny","wannabe coder"]
 def intro():
-	gointor = 1
-	while gointor != 1000:
-		for x in range(random.randint(1,gointor)):
-			print("  ",end="")
-		print("x")
-		time.sleep(random.uniform(0,3/gointor))
-		gointor+=1
-		questens()
-def questens():
 	global name
 	global exoshtion
 	global monkilled
@@ -48,6 +64,39 @@ def questens():
 	global loot
 	global playedmount
 	global coller
+	global health
+	global lootunseen
+	exoshtion = 0
+	rations = 5
+	monkilled = False
+	pythonbridge = False
+	win = False
+	loot = [f"hp{20}"]
+	name = ""
+	playedmount = 0
+	health = 100
+	coller = ""
+	gointor = 1
+	lootunseen = []
+	while gointor != 1000:
+		for x in range(random.randint(1,gointor)):
+			print("  ",end="")
+		print("x")
+		time.sleep(random.uniform(0,3/gointor))
+		gointor+=1
+		questens()
+def questens():
+	global name
+	global lootunseen
+	global exoshtion
+	global monkilled
+	global rations
+	global pythonbridge
+	global win
+	global loot
+	global playedmount
+	global coller
+	global health
 	name = input("whats your name adventurer (type your name then press enter to continue): ")
 	coller = input(f"hello {name} whats your favorite color: ")
 	print(f"""hello {name} who likes the color {coller}, your quest is "To seek the Holy Grail" without reaching exhaustion level 5 good luck""")
@@ -61,23 +110,10 @@ def main():
 	global loot
 	global playedmount
 	global coller
+	global lootunseen
+	global health
 	while True:
-		try:
-			action = int(input(f"""rations left: {rations}
-exhaustion level: {exoshtion}
-what would you like to do
-1 to continue along path
-2 to rest
-3 to hunt for food
-4 to check inventory: """))
-			if action not in [1,2,3,4]:
-				inputcorect()
-			else:
-				break
-		except:
-			inputcorect()
-		if action == 1:
-			rations-=1
+		while True:
 			if rations <0:
 				print("you starved to death and killer guinea pigs ate you")
 				print("game over")
@@ -86,11 +122,32 @@ what would you like to do
 				print("you died of exhaustion and carnivorous antelopes ate your earlobes.")
 				print("game over")
 				break
+			if health < 1:
+				print("you died of... well... nevermind you just die.")
+				print("game over")
+				break
+			try:
+				action = int(input(f"""rations left: {rations}
+exhaustion level: {exoshtion}
+health: {health}
+what would you like to do
+1 to continue along path
+2 to rest
+3 to hunt for food
+4 to check inventory: """))
+				if action not in [1,2,3,4]:
+					inputcorect()
+				else:
+					break
+			except:
+				inputcorect()
+		if action == 1:
+			rations-=1
 			event = random.randint(1,3)
 			if event == 3:
-				eventtype = random.randint(1,4)
+				eventtype = random.randint(1,5)
 				if eventtype == 1:
-					lose = random.randint(0,exoshtion-1)
+					lose = random.randint(0,2)
 					print(f"a blizzard comes thru and you gain {lose} exhaustion",end=" ")
 					exoshtion -= lose
 					lose = random.randint(0,rations-1)
@@ -107,13 +164,12 @@ what would you like to do
 						print("game over")
 						break
 					else:
-						nam = input("What... is your quest?").lower
-						if "grail" not in nam:
+						nam = str(input("What... is your quest: ")).lower
+						if nam != "To seek the Holy Grail":
 							print("wrong *as you are thrown into the casum*")
 							print("you die and are turned into a lemon")
 							print("game over")
 							break
-
 						if playedmount >1:
 							nam = input("What... is the air-speed velocity of an unladen swallow: ").lower
 							if nam == "What do you mean? An African or a European swallow?".lower:
@@ -134,4 +190,51 @@ what would you like to do
 							else:
 								print("you may pass")
 								print("you make it across the bridge")
-	
+				if eventtype == 4:
+					bob = random.randint(1,5)
+					print(f"a flash flood comes in and you take {bob} damage.")
+					health -= bob
+				if eventtype == 5:
+					monhelth = random.randint(50,100)
+					monname = monlist[random.randint(0,7)]
+					print(f"a {monname} appears")
+					while True:
+						if True:
+							unused,unused = print_mon(monhelth,monname)
+							print("1 to heal")
+							print("2 to attack")
+							while True:
+								tur = input()
+								if tur not in ["1","2"]:
+									inputcorect()
+								else:
+									break
+							if tur == 2:
+								damage = random.randint(25,51)
+								if damage == 51:
+									damage = random.randint(60,100)
+								print(f"you deal {damage} damage")
+								monhelth -= damage
+								if monhelth < 1:
+									print("you killed the monster")
+									loot,lootunseen.append(get(raeraty=random.randint(3,5)))
+									print(f"you get a {loot}")
+									break
+							elif tur == 2:
+								iteration = 1
+								print("0 to return")
+								for x in loot:
+									print(f"{iteration}: ",end = "")
+									print(x)
+									iteration += 1
+								while True:
+									try:
+										inp = int(input("what do you want to use"))
+										break
+									except:
+										inputcorect()
+								if inp == 
+									
+					
+
+main()
