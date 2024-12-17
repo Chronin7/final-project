ce4594b36d3c112a538ff25224dc4484a0cf4e20
Chronin7 <liam.perl@ucas-edu.net>
2024-12-17 02:02:16 -0700

    after days of pure pain its done
    THE air-speed velocity of an unladen swallow

--- .py
+++ .py
@@ -1,10 +1,61 @@
-import random, math
 
+import random, math ,os ,termios ,time, multiprocessing, threading
+from typing import List
+
+class Item:
+	"""name:str
+	type:str
+	health:int"""
+	def __init__(self, name:str, type:str, health:int):
+		self.name = name
+		self.type = type
+		self.health = health
+	name:str
+	type:str
+	health:int
+
+class Monster:
+	"""name:str
+	health:int
+	damage:int
+	lootrarity:int
+	loot:list[Item]"""
+	def __init__(self, name, health, damage, lootrarity = 0, loot:List[Item] = []):
+		self.name = name
+		self.health = health
+		self.damage = damage
+		self.lootrarity = lootrarity
+		self.loot = loot
+	name:str
+	health:int
+	damage:int
+	lootrarity:int
+	loot:List[Item]
+
+class Moves:
+	"""north: List[True/False(bool),destination(str)]
+	south :List[True/False(bool),destination(str)]
+	east: List[True/False(bool),destination(str)]
+	west: List[True/False(bool),destination(str)]"""
+	def __init__(self,north,south,east,west):
+		self.north = north
+		self.south = south
+		self.east = east
+		self.west = west
+	north:List[str]
+	south:List[str]
+	east:List[str]
+	west:List[str]
+
+def pprint(text):
+	"""prints green text"""
+	print('\x1b[6;30;42m' + text + '\x1b[0m')
 def getAction(l, zerooption = True):
+	"""gets desired options for X action"""
 	if zerooption:
-		print("0: return")
+		pprint("0: return")
 	for i,action in enumerate(l):
-		print(f'{i+1}: {action}')
+		pprint(f'{i+1}: {action}')
 	while True:
 		try:
 			v = int(input("What do you want: "))
@@ -14,22 +65,26 @@ def getAction(l, zerooption = True):
 				return l[v-1]
 		except:
 			pass
-		print("nope")
+		pprint("nope")
 
 def inventoryCount(type = None):
+	"""counts inventory len returns len of filtered type"""
 	global invin
 	if type == None:
 		return len(invin)
-	return len(filter(lambda item: item["type"] == type, invin))
+	return len(filter(lambda item: item.type == type, invin))
 
 def pickItem(type = None):
+	"""has user pick from inventory"""
 	global invin
 	l = invin
 	if type != None:
-		l = filter(lambda item: item["type"] == type, l)
-	print("0: return")
+		l = filter(lambda item: item.type == type, l)
+	pprint("0: return")
+	for x in filter(lambda item: item.type == "all", l):
+		l.append(x)
 	for i,item in enumerate(l):
-		print(f'{i+1}: {item["name"]}')
+		pprint(f'{i+1}: {item.name}')
 	while True:
 		try:
 			v = int(input("What do you want to use: "))
@@ -41,68 +96,294 @@ def pickItem(type = None):
 				return ret
 		except:
 			pass
+damageNegative=1
 
-
-def applyItem(item, monster):
+def applyItem(item: Item, monster: Monster):
+	"""apply the item effect(s)"""
 	global health
-	match item["name"]:
-		case "RPG":
-			monster["health"] = max(monster["health"]-20, 0)
-		case "holy hand grenade":
-			monster["health"] = 0
-		case "health potion +5":
-			health += 5
-		case "health potion +10":
-			health += 10
-		case "health potion +20":
-			health += 10
-		case "health potion +25":
-			health += 10
-		case "health potion +50":
-			health += 10
-		case "health potion +75":
-			health += 10
-		case "health potion +100":
-			health += 10
-		case "deodorant":
-			if monster["name"]== "shrek":
-				monster["health"] = 0
+	global damageNegative
+	if item.type == "RPG":
+		monster.health = max(monster.health-20, 0)
+	elif item.type == "holy hand grenade":
+		print("""
+
+
+ 0 
+ |/
+ |
+/ \\
+""")
+		for x in range(100):
+			print()
+		time.sleep(.01)
+		print("""
+
+
+ 0      
+ |/
+ |
+/ \\
+""")
+		for x in range(100):
+			print()
+		time.sleep(.01)
+		print("""
+
+
+ 0              
+ |/
+ |
+/ \\
+""")
+		for x in range(100):
+			print()
+		time.sleep(.01)
+		print("""
+
+
+ 0                      
+ |/
+ |
+/ \\
+""")
+		for x in range(100):
+			print()
+		time.sleep(.01)
+		print("""
+
+
+ 0                               
+ |/
+ |
+/ \\
+""")
+		for x in range(100):
+			print()
+		time.sleep(.01)
+		print("""
+
+
+ 0                                      
+ |/
+ |
+/ \\
+""")
+		for x in range(100):
+			print()
+		time.sleep(.01)
+		print("""
+
+
+ 0                                                 
+ |/
+ |
+/ \\
+""")
+		for x in range(100):
+			print()
+		time.sleep(.01)
+		print("""
+
+
+ 0                                                                           
+ |/
+ |
+/ \\
+""")
+		for x in range(100):
+			print()
+		time.sleep(.01)
+		print("""
+
+
+ 0                                                                                                                    
+ |/
+ |
+/ \\
+""")
+		for x in range(100):
+			print()
+			time.sleep(.01)
+		print("""
+
+
+ 0                                                                                                  BOOM              "''"'"
+ |/                                                                                                                ""'000000
+ |                                                                                                                  ""'"0000
+/ \\                                                                                                                  '""'"0
+""")
+		monster.health = 0
+	elif item.type == "health potion +5":
+		health += 5
+	elif item.type == "health potion +10":
+		health += 10
+	elif item.type == "health potion +20":
+		health += 10
+	elif item.type == "health potion +25":
+		health += 10
+	elif item.type == "health potion +50":
+		health += 10
+	elif item.type == "health potion +75":
+		health += 10
+	elif item.type == "health potion +100":
+		health += 10
+	elif item.type ==  "deodorant":
+		if monster.name== "shrek":
+			print('"I love Shrek, so I ordered the DVD and ate it. It tasted terrible and it hurt really bad going down. And when I finally had the relief of it leaving I had some insane diarrhea. Not recommended and shrek was to hot to handle and thicc daddy shrek need to be thicc 3 no golira or moncey we want funny monce shrek hot monke funne" exact movie revue quote')
+			monster.health = 0
+	elif item.type == "15 foot long pole":
+		damageNegative-=15
+	elif item.type == "nuke":
+		inp = input("conferm (this will also kill you the monster and the world) (y/n):")
+		if inp == "y":
+			pprint("all dead")
+			pprint("gameover")
+			health=0
+			monster.health = 0
+	else:
+		print(f" error with apply item got Item:{item} monster:{monster}")
 		
 			
 
-good = ['holy hand gernade one use insta kill','rpg once per battle 20 damage at begining','heth + 50 potion','Sten MK II tends to misfire sometimes has buletts bounce off of tagert +20% damage','Apache Revolver you can use it like a gun (terible aim) a nife (way to flexible) or a iron fist (the only safe way to use it) +30% damage','pickled lepercon head +1 luck for 7 turns','luck potion +1 luck for 2 turns']
+good = ['holy hand grenade one use insta kill','rpg once per battle 20 damage at begining','heth + 50 potion','Sten MK II tends to misfire sometimes has buletts bounce off of tagert +20% damage','Apache Revolver you can use it like a gun (terible aim) a nife (way to flexible) or a iron fist (the only safe way to use it) +30% damage','pickled lepercon head +1 luck for 7 turns','luck potion +1 luck for 2 turns']
 norm = ["nuke (you cant use it cuse it will kill evorything and evoryone including you)","stick + 3% damage",'helth + 10 porion','deodorant (shreck wants it)','luck charm +1 luck for a turn','luck potion +1 luck for 2 turns']
 rearer = ['a peace of a lemon','sord + 10% damage','helth + 20 prtion','slingshot +5% damage','a 15 foot long pole','clover +1 luck for 3 turns','luck potion +1 luck for 4 turns']
 
 def max(a,b):
+	"""returns bigger number"""
 	if a > b:
 		return a
 	return b
+def getloot(rearity):
+	"""returns loot"""
 
 
-invin=[{"name":"health +20 potion","health": 20,"special": None}]
-maps={
-	"grail": {
-		"loot": [
-			{"name": "grail", "health": 0, "special": "win", "type": "other"}
-		],
-		"monster": None
+invin=[Item("health +20 potion", 20,"anywhere")]
+
+maps ={
+	"grail":{"name": "grail",
+	"lootrearity": -100,
+	"movment": {"direction":None, "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"boss":{"name": "gliched cididel",
+	"lootrearity": None,
+	"movment": {"direction":None, "north": None, "south": None, "east": None, "west": "grail"},
+	"monster": Monster("programer", 1000, 50)},
+
+	"glichedplains2":{"name": "gliched plains",
+	"loorearityt": 2,
+	"movment": {"direction":"west" "south", "north": None, "south": "glichedplains1", "east": None, "west": "boss"},
+	"monster": Monster("robot", 300, 50, 3)},
+
+	"glichedplains1":{"name": "gliched plains",
+	"loorearityt": 3,
+	"movment": {"direction":"north" "east", "north": "glichedplains2", "south": None, "east": "mouantan5", "west": None},
+	"monster": Monster("robot", 250, 45, 3)},
+
+	"mountan5":{"name": "mounatn",
+	"loorearityt": 3,
+	"movment": {"direction":"west" "east", "north": None, "south": None, "east": "mountan4", "west": "glichedplains1"},
+	"monster": Monster("robot", 200, 25, 2)},
+
+	"mountan4":{"name": "mountan",
+	"loorearityt": 2,
+	"movment": {"direction":"west" "east", "north": None, "south": None, "east": "mountan3", "west": "mountan5"},
+	"monster": None},
+
+	"mountan3":{
+		"name": "mountan",
+		"loorearityt": 3,
+		"movment": {"direction":"east" "west", "north": None, "south": None, "east": "mountan2", "west": "mountan4"},
+		"monster": Monster("dragon", 100, 50, 3)
 	},
-	"boss": {
-		"loot": [],
-		"monster": {"name": "programer", "health":10000, "attack": 72, "special": "boss", "loot":None },
-		"moves": [
-			{"direction": "east", "dest": "glitch2"},
-			{"direction": "west", "dest": "grail"}
-		]
-	},
+
+	"mountan2":{"name": "mountan",
+	"loorearityt": 3,
+	"movment": {"direction":"west" "east", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+	
+	"mountan1":{"name": "mountan",
+	"loorearityt": 3,
+	"movment": {"direction":"north" "south", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"swamp3":{"name": "swamp",
+	"loorearityt": 2,
+	"movment": {"direction":"north" "south", "north": None, "south": None, "east": None, "west": None},
+	"monster": Monster("shreck", 300, 5, 3)},
+
+	"swamp2":{"name": "swamp",
+	"loorearityt": 2,
+	"movment": {"direction":"east" "south", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"swamp1":{"name": "swamp",
+	"loorearityt": 3,
+	"movment": {"direction":"west" "north", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"viliage":{"name": "french castle (how did you get in here anyway)",
+	"loorearityt": 7298047590287387642980743508,
+	"movment": {"direction":"west" "south", "north": None, "south": None, "east": None, "west": None},
+	"monster": Monster("napolean", 300000000, 50000000, 3719875493876904187549473295)},
+
+	"forest":{"name": "forest",
+	"loorearityt": 3,
+	"movment": {"direction":"north" "south", "north": None, "south": None, "east": None, "west": None},
+	"monster": Monster("killer bunny", 15, 10, 3)},
+
+	"foothills3":{"name": "foot hills",
+	"loorearityt": 2,
+	"movment": {"direction":"west" "east", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"foothills2":{"name": "foot hills",
+	"loorearityt": 2,
+	"movment": {"direction":"east" "south", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"foothills1":{"name": "foot hills",
+	"loorearityt": 3,
+	"movment": {"direction":"west" "north" "east" "south", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"field6":{"name": "field",
+	"loorearityt": 2,
+	"movment": {"direction":"west" "east", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"field5":{"name": "field",
+	"loorearityt": 2,
+	"movment": {"direction":"west" "east", "north": None, "south": None, "east": None, "west": None},
+	"monster": Monster("flying snake", 50, 10, 2)},
+
+	"field4":{"name": "field",
+	"loorearityt": 1,
+	"movment": {"direction":"south", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"field3":{"name": "field",
+	"loorearityt": 2,
+	"movment": {"direction":"east" "north", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"field2":{"name": "field",
+	"loorearityt": 1,
+	"movment": {"direction":"west" "east", "north": None, "south": None, "east": None, "west": None},
+	"monster": None},
+
+	"field1":{"name": "field",
+	"loorearityt": 2,
+	"movment": {"direction":"north" , "north": None, "south": None, "east": None, "west": None},
+	"monster": Monster("robot", 300, 50, 3)}
 }
-
+	
 # player stats
 damageBuff = 1
 health = 100
 damageBeforeBattle = 0
-location = "boss"
+location = "field4"
 luck = 0
 
 while True:
@@ -122,18 +403,115 @@ while True:
 				if item != None:
 					continue
 				else:
+					applyItem(item,monster)
 					
 					
 			
-		print(f'{monster["name"]} defeated')
+		pprint(f'{monster["name"]} defeated')
 		if monster["loot"] != None:
 			loot = monster["loot"]
 			invin.append(loot)
-			print(f'{monster["name"]} dropped {loot["name"]}. Its in your inventory')
+			pprint(f'{monster["name"]} dropped {loot["name"]}. Its in your inventory')
 		currentLocation["monster"] = None
 
 
 				
 
 				
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+class tom:
+	def __init__(self):
+		pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- final_loot.py
+++ final_loot.py
@@ -1,12 +1,333 @@
-from final_ascii import *
-def loot(raeraty):
-	if raeraty == 1:
-		return "heath potion",5
-	if raeraty == 2:
-		return "heath potion",10
-	if raeraty == 3:
-		return "heath potion",20
-	if raeraty == 4:
-		return "heath potion",50
-	if raeraty == 5:
-		return "heath potion",100
+from dataclasses import dataclass
+from copy import copy, deepcopy
+import time, random
+from typing import List, Dict, Tuple
+@dataclass
+class Monster:
+	name:str
+	health:str
+	damage:str
+	lootLevel:int = 1
+
+@dataclass
+class Item:
+	name:str
+	health:int = 0
+	damagebuff:float = 0
+	predamage:int = 0
+	luck:int = 0
+	life:int = None
+	type:str = "consumable"
+
+@dataclass
+class Moves:
+	north:str = None
+	south:str = None
+	east:str = None
+	west:str = None
+	def selectMove(self):
+		return selectOption([
+			("North", self.north),
+			("South", self.south),
+			("East", self.east),
+			("West", self.west)
+		])
+
+@dataclass
+class Location:
+	description:str
+	moves:Moves
+	itemRarity:int = 1
+	hasLoot:bool = True
+	monster:Monster = None
+
+#best
+goodLoot = [
+	Item("Holy Hand Grenade: instakill", damagebuff=100000000, type="consumable"),
+	Item("RPG: once per battle 20 damage at beginning", predamage=20, type="weapon"),
+	Item("Health + 50 Potion", health=50, type="consumable"),
+	Item("Sten MK II: tends to misfire sometimes has bullets bounce off of target +20% damage", damagebuff=.20, type="weapon"),
+	Item("Apache Revolver: you can use it like a gun (terrible aim) a knife (way too flexible) or an iron fist (the only safe way to use it) +30% damage", damagebuff=.30, type="weapon"),
+	Item("Pickled Leprechaun Head: +1 luck for 7 turns", life=7, type="timed"),
+	Item("Luck Potion: +1 luck for 10 turns", life=10, type="timed")
+]
+#worst
+norm = [
+	Item("Nuke Kill All", health=-100000000,damagebuff=34517089, type="consumable"),
+	Item("Stick +5% damage", damagebuff=.5, type="weapon"),
+	Item("Health +10 potion", health=10, type="consumable"),
+	Item('Deodorant', type='consumable'),
+	Item(name='luck charm +1 luck for a turn', life=1, type="timed"),
+	Item(name='luck potion +1 luck for 2 turns', life=2, type="timed")
+]
+#meh
+rearer = [
+	Item('A Peace of a Lemon', type='consumable'),
+	Item('Sword + 10% damage', damagebuff=.1, type='weapon'),#damageBuff ?
+	Item('Health + 20 potion', health=20, type='consumable'),
+	Item('Slingshot +5% damage', damagebuff=.05, type='weapon'),
+	Item('A 15 foot long pole', health=-2, type='consumable'),
+	Item('clover +1 luck for 3 turns', luck=1, type='consumable'),
+	Item('luck potion +1 luck for 4 turns', luck=1, type='consumable')]
+
+
+worldTemplate = {
+	"start": Location(description="French castle (how did you get in here anyway hon hon hon)", itemRarity=3, moves=Moves(east="field4")),
+	"field1": Location(description="You are in a field of grass",itemRarity=1,moves=Moves(north="foothills1")),
+	"field2": Location(description="You are in a field of goat heads", itemRarity=1,moves=Moves(west="field3",east="foothills1")),
+	"field3": Location(description="You are in a field of mud",itemRarity=2,moves=Moves(north="forest",east="field2")),
+	"field4": Location(description="You are in a field of molten rice",itemRarity=1, moves=Moves(south="forest")),
+	"field5": Location(description="You are in a field of grass",itemRarity=2,moves=Moves(west="foothill1",east="field6"), monster=Monster(name="flying snake", health=50, damage=10, lootLevel=3)),
+	"field6": Location(description="You are in a field of coconuts",itemRarity=2,moves=Moves(north="foothills2", west="field5")),
+	"foothills1": Location(description="you are in a foothills biome",itemRarity=3,moves=Moves(north="mountain1",south="field1",east="field2",west="field5")),
+	"foothills2": Location(description="you are in a foothills biome", itemRarity=2,moves=Moves(east="foothills3", south="field6")),
+	"foothills3": Location(description="you are in a foothills biome", itemRarity=2,moves=Moves(east="swamp1",west="foothills2")),
+	"forest": Location(description="you in big fat forest", itemRarity=3,moves=Moves(north="field4",south="field3"), monster=Monster(name="killer bunny", health=15, damage=10, lootLevel=3)),
+	"swamp1": Location(description="yucky swamp you in hmmmm?", itemRarity=3,moves=Moves(north="swamp3",west="foothills3")),
+	"swamp2": Location(description="in yucky swamp am i hmmmm?", itemRarity=2,moves=Moves(west="mountain2", south="swamp3")),
+	"swamp3": Location(description="swamp yucky in both are we hmmmm?", itemRarity=3,moves=Moves(north="swamp2",south="swamp1"), monster=Monster(name="shreck", health=300, damage=2, lootLevel=3)),
+	"mountain1": Location(description="you are in a mountain", itemRarity=3,moves=Moves(north="BOD", south="foothills1")),
+	"mountain2": Location(description="you are in a mountain", itemRarity=3,moves=Moves(west="mountain3", east="swamp2")),
+	"mountain3": Location(description="you are in a mountain", itemRarity=3,moves=Moves(west="mountain4", east="mountain2"), monster=Monster(name="dragon", health=100, damage=50, lootLevel=3)),	
+	"mountain4": Location(description="you are in a mountain", itemRarity=2,moves=Moves(west="mountain5", east="mountain3"), monster=Monster(name="robot", health=50, damage=10, lootLevel=3)),
+	"mountain5": Location(description="you are in a mountain", itemRarity=3,moves=Moves(west="glitchedPlains1", east="mountain4"), monster=Monster(name="robot", health=200, damage=20, lootLevel=3)),
+	"BOD": Location(description="BODY", hasLoot=False, moves=Moves(north="moun1tain5")),
+	"glitchedPlains1": Location(description="You have entered the glitched plains", itemRarity=3, moves=Moves(north="glitchedPlains2", east="mountain5"), monster=Monster(name="robot", health=250, damage=50, lootLevel=3)),
+	"glitchedPlains2": Location(description="You have entered the glitched plains", itemRarity=2, moves=Moves(west="glitchedCitadel", south="glitchedPlains1"), monster=Monster(name="robot", health=300, damage=50, lootLevel=3)),
+	"glitchedCitadel": Location(description="You have entered the glitched citadel. This is where the boss is good luck from the game itsleff", hasLoot=False, moves=Moves(west="holyGrail", east="glitchedPlains2"), monster=Monster(name="The Programer", health=1000, damage=50)),
+	"holyGrail": Location(description="grail",hasLoot=False, moves=Moves())
+}
+
+world: Dict[str,Location]
+location: Location
+inventory: List[Item]
+health: int
+damage: int
+color: str
+name: str
+quest = "To seek the Holy Grail"
+playedAmount = 0
+
+def initGame():
+	global world
+	global location
+	global inventory
+	global health
+	global damage
+	global color
+	global name
+	global playedAmount
+	world = deepcopy(worldTemplate)
+	location = world["field4"]
+	inventory = []
+	health = 100
+	damage = 5
+	name = input("whats your name adventurer (type your name then press enter to continue): ")
+	color = input(f"hello {name} whats your favorite color: ")
+	print(f"""hello {name} who likes the color {color}, your quest is "{quest}" good luck""")
+	playedAmount+=1
+	
+def move(loc: str):
+	global location
+	global inventory
+	for item in [item for item in inventory if item.life != None]:
+			item.life -= 1
+			if item.life == 0:
+				print(f"Your {item.name} has expired")
+				inventory.remove(item)
+	location = world[loc]
+
+def selectOption(options: List[Tuple[str,any]], cancelable = True, noOptionText = "You can't do that.") -> any:
+	options = [x for x in options if x[1] != None]
+	if len(options) == 0:
+		print(noOptionText)
+		return None
+	while True:
+		try:
+			print("Select an option:")
+			if cancelable:
+				print("0. Cancel")
+			for i, option in enumerate(options):
+				print(f"{i+1}. {option[0]}")
+			value = int(input("Choose wisely: "))
+			print()
+			if cancelable and value == 0:
+				return None
+			elif value < 1 or value > len(options):
+				print("try again")
+			else:
+				return options[value-1][1]
+		except ValueError:
+			print("Invalid input")
+
+def randomItem(rarity = 0):
+	if sum([item.luck for item in inventory]) > 0:
+		rarity += 1
+	if rarity == 1:
+		return copy(random.choice(norm))
+	elif rarity ==2:
+		return copy(random.choice(rearer))
+	elif rarity ==3:
+		return copy(random.choice(goodLoot))
+	elif rarity ==4:
+		return Item("ball of thorns", health=-50 ,type="consumable")
+	raise Exception(f"Invalid rarity {rarity}")
+
+def useItem():
+	global health
+	global inventory
+	item:Item = selectOption([(item.name, item) for item in inventory if item.type == "consumable"], noOptionText="You have no items to use")
+	if item == None:
+		return False
+	health += item.health
+	if item.health > 0:
+		print(f"You used {item.name} and gained {item.health} health")
+	else:
+		print(f"You used {item.name} and lost {-item.health} health")
+	print(f"You have {health} health left")
+	inventory.remove(item)
+
+def BOD():
+	global name
+	global playedAmount
+	global color
+	nam = input("""you come to a rope bridge spanning a casum and a man stops you and says "Stop. Who would cross the Bridge of Death must answer me these questions three, ere the other side he see. What... is your name: """)
+	if nam.lower != name.lower:
+		print("wrong *as you are thrown into the casum")
+		print("you die and aliens take your body and are diapointed that you cant play poker")
+		print("wrong *as you are thrown into the casum*")
+		print("you die and aliens take your body and are disappointed that you cant play poker")
+		print("game over")
+		raise "dead"
+	else:
+		nam = str(input("What... is your quest: "))
+		if nam.lower().strip() != quest.lower().strip():
+			print("wrong *as you are thrown into the casum*")
+			print("you die and are turned into a lemon")
+			print("game over")
+			return "dead"
+		if playedAmount >1:
+			nam = input("What... is the air-speed velocity of an unladen swallow: ").lower
+			if nam == "What do you mean? An African or a European swallow?".lower:
+				print(" Huh? I... I don't know that. AUUUUUUUGGGGGGGGGGGHHH!!")
+				print("you sucsesfully make it across the bridge")
+				print(" Huh? I... I don't know that. AUUUUUUUGGGGGGGGGGGHHH!! *as he is thrown into the casum*")
+				print("you successfully make it across the bridge")
+				return "mountain5"
+			else:
+				print("wrong *as you are thrown into the casum")
+				print("you die and joe takes your apendix")
+				print("wrong *as you are thrown into the casum*")
+				print("you die and a goat gives you a wet willy")
+				print("game over")
+				return "dead"
+		else:
+			nam = input("What... is your favourite colour: ").lower
+			nam = input("What... is your favorite colour: ").lower
+			if nam != color:
+				print("wrong *as you are thrown into the casum*")
+				print("you die and billy the bird makes you into a nest")
+				print("game over")
+				return "dead"
+			else:
+				print("you may pass")
+				print("you make it across the bridge")
+				return "mountain5"
+				
+def holyGrail():
+	print("You have found the Holy Grail!")
+	if input("do you drink(y/n):").lower=="y":
+		print("""Traceback (most recent call last):
+  File "/Users/allenperl/personal/pythonPlay/game.py", line 252, in <module>
+    item = randomItem(location.itemRarity)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/Users/allenperl/personal/pythonPlay/game.py", line 106, in randomItem
+    raise Exception(f"Invalid rarity {rarity}")
+Exception: Invalid rarity 0""")
+		time.sleep(3)
+		print("BACK FROM THE DEAD CODE")
+		print("P.S. you win")
+		return "dead"
+	else:
+		return "dead"
+
+
+def totalDamage():
+	"""returns total damage"""
+	return (sum([item.damagebuff for item in inventory ])+1)*damage
+
+def battle():
+	global health
+	global inventory
+	global location
+
+	monster = location.monster
+	print(f"A {monster.name} appears!")
+	while True:
+		option = selectOption([
+			("Fight", "fight"),
+			("Use Item", "useItem")
+		], cancelable=False)
+		if option == "fight":
+			print(f"You fight the {monster.name}")
+			attack = totalDamage()
+			print(f"You deal {attack} damage")
+			monster.health = max(0, location.monster.health - attack)
+			if monster.health <= 0:
+				print(f"You have defeated the {monster.name}")
+				item = randomItem(monster.lootLevel)
+				print(f"{monster.name} dropped a {item.name} and you picked it up")
+				inventory.append(item)
+				location.monster = None
+				return True
+			print(f"The {monster.name} has {monster.health} health left")
+		elif option == "useItem":
+			if useItem() == False:
+				continue
+		print(f"{location.monster.name} attacks!")
+		health -= location.monster.damage
+		if health <= 0:
+			print("You have died and a antelope ate your earlobes")
+			return False
+		print(f"You have {health} health left")
+
+playedAmount =0
+while True:
+
+	if input("do you want to play(y/n)")!="y":
+		break
+
+	initGame()
+
+	while True:
+		if location.description == "BODY":
+			if BOD()=="dead":
+				break
+			else:
+				move("mountain5")
+		elif location.description == "grail":
+			holyGrail()
+		else:
+			print(location.description)
+			if location.monster:
+				if battle() == False:
+					break
+			option = selectOption([
+				("Move", "move"),
+				("Search for loot", "search"),
+				("Use Item", "useItem")
+			], False)
+			if option == "useItem":
+				useItem()
+			elif option == "search":
+				if location.hasLoot:
+					item = randomItem(location.itemRarity)
+					location.hasLoot = False
+					inventory.append(item)
+					print(f"You found a {item.name}")
+				else:
+					print("You found nothing")
+			elif option == "move":
+				place = location.moves.selectMove()
+				if place:
+					move(place)
